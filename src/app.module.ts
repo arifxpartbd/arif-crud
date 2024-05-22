59d945c69a455668d3a7c28ec48829b0ea6d651b
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-22 16:46:55 +0600

    database created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (conigService: ConfigService) => ({
+        type: 'postgres',
+        host: conigService.get('DB_HOST'),
+        port: +conigService.get('DB_PORT'),
+        username: conigService.get('DB_USERNAME'),
+        password: conigService.get('DB_PASSWORD'),
+        database: conigService.get('DB_NAME'),    
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        //do not use synchronize : true in real project
+        synchronize: true,
+      }),
+    }),
+    CitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
